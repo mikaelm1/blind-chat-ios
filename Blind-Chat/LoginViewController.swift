73a33632ff07aac915b21f0599fbd6c635bfb40f
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-10-06 20:07:57 -0700

    Initial commit

---
+++ Blind-Chat/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Blind-Chat
+//
+//  Created by Mikael Mukhsikaroyan on 10/6/16.
+//  Copyright © 2016 MSquaredmm. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = FlatGreen()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("MEMORY WARNING")
+    }
+
+
+}
+
